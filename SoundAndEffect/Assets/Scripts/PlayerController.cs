a977ff29077f756bc59c7c0e1f64b7f8341fd4c7
PSYCHE-707 <michpark200519@gmail.com>
2023-03-24 11:42:19 -0700

    Finished tutorial 2 almost t3
    Left off on fixing the jumping animations

--- SoundAndEffect/Assets/Scripts/PlayerController.cs
+++ SoundAndEffect/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
         
     }
 }
